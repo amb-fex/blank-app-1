c895b1cc313a52ddee28b43ca77825deeeb82ced
amb-fex <sdefex@amb.cat>
2025-03-06 12:07:32 +0100

    Update 2_Productes.py

--- pages/2_Productes.py
+++ pages/2_Productes.py
@@ -3,6 +3,10 @@ from sqlalchemy import create_engine
 import pandas as pd
 import plotly.express as px
 
+st.set_page_config(
+    layout="wide",
+)
+
 st.markdown(
     """
     <style>
@@ -10,14 +14,7 @@ st.markdown(
             width: 100px !important; # Set the width to your desired value
             background-color: rgba(255, 255, 255, 0.0);  /* Transparent background */
         }
-    </style>
-    """,
-    unsafe_allow_html=True,
-)
-
-st.markdown(
-    """
-    <style>
+    
         .stAppHeader {
             font-size: 10px;  /* Tamaño del título */
             background-color: rgba(255, 255, 255, 0.0);  /* Transparent background */
@@ -25,17 +22,26 @@ st.markdown(
         }
 
         .block-container {
+            margin:1rem;
             padding-top: 0rem;
             padding-bottom: 0rem;
-            padding-left: 0rem;
-            padding-right: 0rem;
+            padding-left: 1rem;
+            padding-right: 1rem;
         }
+
+        
+        /* Modificar el tamaño de los checkboxes en el multiselect */
+        div[data-baseweb="checkbox"] {
+            transform: scale(0.5);  /* Ajusta el tamaño */
+        }
+    
         h2 {
             text-align: center;  
         }
     </style>
     """, unsafe_allow_html=True,
 )
+
 
 
 st.header("📊 Productes")
@@ -46,8 +52,6 @@ def run_query(query):
 
 engine = create_engine(f"postgresql://postgres.spdwbcfeoefxnlfdhlgi:chatbot2025@aws-0-eu-central-1.pooler.supabase.com:6543/postgres")
 
-# Configuración de la aplicación Streamlit
-st.markdown("Mapas de Calor")
 
 # Consulta SQL para obtener la lista de productos
 query_productos = "SELECT DISTINCT nomproducte FROM public.descargas;"
@@ -59,6 +63,8 @@ col1, col2 = st.columns([3, 1])  # La primera columna es más ancha que la segun
 
 # Controles en la segunda columna
 with col2:
+    st.markdown("")
+    st.markdown("")
     st.markdown("Controles: Mapa de calor de descàrregues un producte")
     
     # Selector de productos
@@ -156,11 +162,18 @@ else:
 
 # Ahora hago el gráfico que me deja seleccionar más de un producto para la comparación
 with col1:
-    st.markdown('Mapa de calor de descàrregues per a més de un producte')
+  #  st.markdown('Mapa de calor de descàrregues per a més de un producte')
 
-querry_productos_mes_a_mes = "SELECT DATE_TRUNC('month', fechadescarga) AS mes, nomproducte, COUNT(*) AS total_descargas FROM public.descargas GROUP BY mes, nomproducte ORDER BY mes, nomproducte;"
+    querry_productos_mes_a_mes = "SELECT DATE_TRUNC('month', fechadescarga) AS mes, nomproducte, COUNT(*) AS total_descargas FROM public.descargas GROUP BY mes, nomproducte ORDER BY mes, nomproducte;"
 
-df_descargas = run_query(querry_productos_mes_a_mes)
+    df_descargas = run_query(querry_productos_mes_a_mes)
+
+    with st.container():  # Full-width container
+            productos_seleccionados = st.multiselect(
+            "Selecciona els productes",
+            options=productos,
+            default=productos[54:60]  
+         )
 
 if not df_descargas.empty:
     # Convertir la columna 'mes' a formato de fecha
@@ -169,19 +182,20 @@ if not df_descargas.empty:
     # Obtener la lista única de productos y meses
     productos = df_descargas["nomproducte"].unique()
     meses = df_descargas["mes"].dt.strftime('%Y-%m').unique()  # Convertir fechas a cadenas de texto
-
+    
+    
+    
     # Controles para seleccionar productos y meses
     with col2:
-        st.markdown("Controles: Mapa de calor de descàrregues per a més de un producte")
-        productos_seleccionados = st.multiselect(
-            "Selecciona els productes",
-            options=productos,
-            default=productos[:10]  
-        )
+        st.header("")
+        st.markdown("")
+        st.markdown("")
+        st.markdown("Controles per comparar més de un producte")
+        
         meses_seleccionados = st.multiselect(
             "Selecciona els mesos",
             options=meses,
-            default=meses[:12]  # Meses seleccionados por defecto
+            default=meses[-12:]  # Meses seleccionados por defecto
         )
 
         # Control para seleccionar el esquema de colores
@@ -205,12 +219,13 @@ if not df_descargas.empty:
         fig = px.imshow(
             df_filtrado.pivot(index="nomproducte", columns="mes", values="total_descargas"),
             labels=dict(x="", y="", color=""),
-            title="Mapa de Calor: Descàrregues per Mes i Producte",
+            title=f"Descàrregues de {productos_seleccionados}",
+            
             color_continuous_scale=esquema_colores2,
-            width=1000,  
-            height=800
+            width=800,
+            height=600
         )
-
+        
         # Mostrar el gráfico en la primera columna
         with col1:
             st.plotly_chart(fig, use_container_width=True)
@@ -221,4 +236,5 @@ if not df_descargas.empty:
 else:
     with col1:
         st.warning("No hi ha dades disponibles per mostrar el mapa de calor.")
+
 
